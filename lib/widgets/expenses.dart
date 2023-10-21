9ee3b7b1790a9e1e9a947d6d6308599a636b252f
timothybaraka <timothybaraka44@gmail.com>
2023-10-21 09:27:08 +0300

    Create the project and landing page

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,56 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+import '../models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Flutter Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure)
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          //Toolbar withe add widget
+
+          const Text('The Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
+        ],
+      ),
+    );
+  }
+}
