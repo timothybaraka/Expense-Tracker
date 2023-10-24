7aae04438bf5bacfcfd50b8563efa2b2deccebf8
timothybaraka <timothybaraka44@gmail.com>
2023-10-24 06:43:27 +0300

    Add a chart to track each category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -33,3 +33,25 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList(); //an extra constructor function apart from the def
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum = sum + expense.amount;
+    }
+
+    return sum;
+  }
+}
